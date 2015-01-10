f14602c445c1fe4d496c1e02c5f6233412a9380f
tao.zeng <taozeng0915@gmail.com>
2015-01-10 14:36:51 +0800

    added the new project

---
+++ project/PuzzleGame/Assets/Scripts/RobotController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+
+	public float maxSpeed = 10.0f;
+	bool facingRight = true;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float move = Input.GetAxis("Horizontal");
+		rigidbody2D.velocity = new Vector2 (maxSpeed * move, rigidbody2D.velocity.y);
+
+	}
+
+	void flip()
+	{
+		facingRight = !facingRight;
+		Vector3 curScale = transform.localScale;
+		curScale.x *= -1;
+
+		transform.localScale = curScale;
+	}
+}
